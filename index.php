bba099bcbfa3152fa6e2a21b7e28cdd9940d450e
ahsamimtiaz <ahsamimtiaz@gmail.com>
2020-12-25 14:03:18 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body style="height: 100vh; background-color: #000">
+<?php
+header("loation:http://youtube.com/");
+?>
+</body>
+</html>
